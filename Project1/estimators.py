92b94f792fd83f249297c270ac71b415e5a3e618
Thanh <binnhatthanhtran@gmail.com>
2023-01-24 18:00:46 -0800

    update custom regressor

--- Project1/estimators.py
+++ Project1/estimators.py
@@ -3,35 +3,74 @@ import numpy as np
 
 
 class CustomRegressors():
-    def __init__(self, obj_func, num_param, grad_obj_func=None, type='normal', num_epochs=10) -> None:
-        self.obj_func = obj_func
-        self.num_param = num_param
-        self.grad_obj_func = grad_obj_func
+    def __init__(self, obj_func_parts, type='normal', gd_params=None) -> None:
+        self.obj_func_parts = obj_func_parts
         self.type = type
-        self.num_epochs = num_epochs
-
+        self.arr_params = None
+        self.bln_fitted =  False
+        self.gd_params = gd_params
+        self._set_num_param()
     def fit(self, x, y):
 
         y = y.reshape(-1)
+        x = x.reshape(-1)
         data_size = len(y)
-        if type == 'normal': # Solving the normal equation exactly    
-            pass
-        elif type == 'gd':
-            pass
-        elif type == 'sgd':
-            pass
+        if self.type == 'normal': # Solving the normal equation exactly    
+            self.arr_params = self._normal_solver(x, y, data_size).T
+        elif self.type == 'gd':
+            self._grad_descent_solver(x, y, data_size)
+        elif self.type == 'sgd':
+            self._sto_grad_descent_solver(x, y, data_size)
         else:
             Exception(f"Unregcognize type of solver: {type}")
+
+        self.bln_fitted = True
+
+    def pred(self, x):
+        if self.bln_fitted:
+            x = x.reshape(-1)
+            matA = np.array(self.obj_func_parts(x)).T
+            return matA@self.arr_params
+
+        else:
+            print("Need to fit before predict.")
+            return None
+
+    def _normal_solver(self, x, y, data_size):
+        matA = np.array(self.obj_func_parts(x)).T
+
+        return np.linalg.inv(matA.T.dot(matA)).dot(matA.T).dot(y)
+
+    def _grad_descent_solver(self, x, y, data_size):
+        num_epochs = self.gd_params["num_epochs"]
+        lr = self.gd_params["lr"]
+        matA = np.array(self.obj_func_parts(x)).T
+        y = y.reshape(len(y),1)
+
+        self.arr_params = np.random.randn(self.num_param, 1)
+        for ep in range(num_epochs):
+            self.arr_params = self.arr_params - lr*matA.T@(matA@self.arr_params-y)
+
+    def _sto_grad_descent_solver(self, x, y, data_size):
+        num_epochs = self.gd_params["num_epochs"]
+        lr = self.gd_params["lr"]
+        batch_size = self.gd_params["batch_size"]
+
+        matA = np.array(self.obj_func_parts(x)).T
+        y = y.reshape(len(y),1)
+        self.arr_params = np.random.randn(self.num_param, 1)
+        for ep in range(num_epochs):
+            shuffled_idx = np.random.permutation(data_size)
+            matA_shuffled = matA[shuffled_idx]
+            y_shuffled = y[shuffled_idx]
+            for i in range(0, data_size, batch_size):
+                matAi = matA_shuffled[i:i+batch_size]
+                yi = y_shuffled[i:i+batch_size]
+                grad = 2/batch_size*matAi.T@(matAi@self.arr_params - yi)
+                self.arr_params = self.arr_params - lr*grad
     
-    def normal_solver(self, x, y, data_size):
-        matA = np.c_[np.ones((data_size,1)), x]
-        
-
-    def grad_descent_solver(self):
-        pass
-
-    def sto_grad_descent_solver(self):
-        pass
+    def _set_num_param(self):
+        self.num_param = self.obj_func_parts(0).size
 
     
 
--- Project1/test.py
+++ Project1/test.py
@@ -1,6 +1,58 @@
 
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from estimators import CustomRegressors
+import math
+from sklearn.model_selection import train_test_split
+
+# f = lambda x: x[0] + x[1]
+# g = lambda x: (x[0],x[0]+x[1])
+# h = lambda x: (x**x, x + 1)
+# print(f((1,1)))
+# print(g((1,1)))
+# print(h(np.array([1,2])))
+
+# print(np.array(h(np.array([[1],[2]]))))
 
 
-f = lambda x: x[0] + x[1]
 
-print(f((1,1)))
+# t=2*np.random.rand(100,1) # unif. distributed on [0,2]
+# t = np.sort(t, axis=0)
+# y=4+3*t+np.random.randn(100,1);
+
+# g = lambda x: np.array([x**0, x, x**2, x**3, np.exp(x)])
+
+data = np.loadtxt("CaCovidInfMarch24toMidJuly.txt")
+t = np.arange(1,len(data),1)
+
+X_train, X_test, y_train, y_test = train_test_split(t, data, train_size=90, shuffle=False)
+
+def f(x): return np.array([x**0, x])
+
+gd_params = {
+    "lr": 0.001,
+    "num_epochs": 100,
+    "batch_size": 20
+}
+
+linear_reg = CustomRegressors(f, type='sgd', gd_params=gd_params)
+linear_reg.fit(X_train,y_train)
+print(linear_reg.arr_params)
+y_pred = linear_reg.pred(t)
+plt.plot(X_train, y_train, "b.")
+plt.plot(X_train,y_pred, "r-")
+plt.xlabel("time", fontsize=18)
+plt.ylabel("y", fontsize=18)
+plt.show()
+
+
+# l = list(range(10))
+# d = {str(a): ord(str(a)) for a in l}
+# def dummy(*args, lr, ne, algo):
+#     print(*args)
+#     print(*kwargs)
+#     print(kwargs)
+
+
+# dummy(*l, **d)
